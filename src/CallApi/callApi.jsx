b477e5a09e97e1695871ad927d069804a092eea9
Alexandre Six <alexandresix2@gmail.com>
2022-01-25 18:31:20 +0100

    Modification du ReadMe

--- src/CallApi/callApi.jsx
+++ src/CallApi/callApi.jsx
@@ -18,65 +18,69 @@ export const DashboardContext = createContext()
  */
 export const DashboardContextProvider=({ children })=>{ 
     const {id}=useParams()
-    console.log(id)
+    console.log(id)        
     const [user, setUser] = useState()
     const [activity, setActivity] = useState()
     const [averageSessions, setAverageSessions] = useState()
     const [performance, setPerformance] = useState()
     const [apiError, setApiError] = useState(false)
-    
-    const getUser = () => {
-        fetch(`${apiUrl}/user/${currentUser}`)
-            .then((response => response.json()))
-            .then(data => setUser(data.data))
-            .catch((error) => setApiError(true))
-    }
 
-    useEffect(() => {
-        if (simDataLoadingTime === 0) getUser()
-        else setTimeout(getUser, simDataLoadingTime)
-    }, [])
-
-    const getUserActivity = () => {
-        fetch(`${apiUrl}/user/${user.id}/activity`)
-            .then((response => response.json()))
-            .then(data => setActivity(data.data.sessions))
-            .catch((error) => setApiError(true))
-    }
-
-    const getUserAverageSessions = () => {
-        fetch(`${apiUrl}/user/${user.id}/average-sessions`)
-            .then((response => response.json()))
-            .then(data => setAverageSessions(data.data.sessions))
-            .catch((error) => setApiError(true))
-    }
-
-
-    const getUserPerformance = () => {
-        fetch(`${apiUrl}/user/${user.id}/performance`)
-            .then((response => response.json())) 
-            .then(data => {
-                setPerformance(data.data.data)
-            })
-            .catch((error) => setApiError(true))
-    }
-
-    useEffect(() => {
-        if (!user) return
-        const getUserData = () => {
-            getUserActivity()
-            getUserAverageSessions()
-            getUserPerformance()
+    /*if(id){*/
+        const getUser = () => {
+            fetch(`${apiUrl}/user/${currentUser}`)
+                .then((response => response.json()))
+                .then(data => setUser(data.data))
+                .catch((error) => setApiError(true))
         }
-        if (simDataLoadingTime === 0) getUserData()
-        else setTimeout(getUserData, simDataLoadingTime)
-    }, [user])
 
+        useEffect(() => {
+            if (simDataLoadingTime === 0) getUser()
+            else setTimeout(getUser, simDataLoadingTime)
+        }, [])
+
+        const getUserActivity = () => {
+            fetch(`${apiUrl}/user/${user.id}/activity`)
+                .then((response => response.json()))
+                .then(data => setActivity(data.data.sessions))
+                .catch((error) => setApiError(true))
+        }
+
+        const getUserAverageSessions = () => {
+            fetch(`${apiUrl}/user/${user.id}/average-sessions`)
+                .then((response => response.json()))
+                .then(data => setAverageSessions(data.data.sessions))
+                .catch((error) => setApiError(true))
+        }
+
+
+        const getUserPerformance = () => {
+            fetch(`${apiUrl}/user/${user.id}/performance`)
+                .then((response => response.json())) 
+                .then(data => {
+                    setPerformance(data.data.data)
+                })
+                .catch((error) => setApiError(true))
+        }
+
+        useEffect(() => {
+            if (!user) return
+            const getUserData = () => {
+                getUserActivity()
+                getUserAverageSessions()
+                getUserPerformance()
+            }
+            if (simDataLoadingTime === 0) getUserData()
+            else setTimeout(getUserData, simDataLoadingTime)
+        }, [user])
+
+        
+   /* }else{
+    }*/
     return (
-        <DashboardContext.Provider value={{user, activity, averageSessions, performance, apiError}}>
-            {children}
-        </DashboardContext.Provider>
-    )
+            <DashboardContext.Provider value={{user, activity, averageSessions, performance, apiError}}>
+                {children}
+            </DashboardContext.Provider>
+        )
 }
 DashboardContextProvider.propTypes = {
     children: propTypes.element.isRequired
